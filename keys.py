b002ccb3880490a89854f80a692bfe6b15e7009b
Jeremias <feronest@yahoo.com>
2021-11-10 21:14:42 +0100

    Heroku

---
+++ keys.py
@@ -0,0 +1,6 @@
+weatherKey = "65ee9edb7a7e7a3186aea38f8429dc84"
+weatherUrl = "http://api.openweathermap.org/data/2.5/weather?"
+discordKey = "NzQ0MjY3MDI4NDI0Mjk0NTAx.Xzgurw.NLm-QLdN-0eWFVzkBd4d33kFW7I"
+WIKI_REQUEST = 'http://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles='
+DATABASE_URL = "postgres://jyavuexntpleai:a59445de852c4a37b929184df6951382e66360f2d83d4c1beec92acc43c9e86a@ec2-54-154-101-45.eu-west-1.compute.amazonaws.com:5432/d4pusr7nfe3h6a"
+WOLFRAM_KEY = "THG7TR-W7WL7K97XK"
